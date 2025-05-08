f7208c761f514dd0850985f3795ea69a6fb7ad49
Ben <Bendavcorr@Gmail.com>
2025-05-08 22:02:14 +0100

    Wander experimentation

---
+++ Ant_Behaviour/scenes/ant_base.gd
@@ -0,0 +1,27 @@
+extends Node3D
+@export var spawn_interval: float = 0.1       # seconds between spawns
+@export var spawn_position: Vector3 = Vector3.ZERO
+@export var ant_scene: PackedScene
+
+var antCount: int
+
+func _ready(): 
+	# Spawn timer setup
+	var spawn_timer := Timer.new()
+	spawn_timer.wait_time = spawn_interval
+	spawn_timer.one_shot = false
+	spawn_timer.autostart = true
+	add_child(spawn_timer)
+	spawn_timer.timeout.connect(Callable(self, "_spawn_ant"))
+
+func _spawn_ant() -> void:
+	if not ant_scene:
+		push_warning("No Ant scene assigned for spawning!")
+		return
+		
+	print('spawning ant')
+	var ant = ant_scene.instantiate()
+	add_child(ant)
+	antCount += 1
+	# Position new ant at spawn point
+	ant.global_transform.origin = spawn_position
---
+++ Ant_Behaviour/test.gd
@@ -0,0 +1,25 @@
+# World.gd
+extends Node
+class_name World
+
+@onready var ant_base = $AntBase
+
+
+@export var show_fps: bool = true
+var _fps_label: Label
+
+func _ready() -> void:
+	print(ant_base.antCount)
+	if show_fps:
+		var fps_layer := CanvasLayer.new()
+		add_child(fps_layer)
+
+		_fps_label = Label.new()
+		_fps_label.text = "FPS: 0"
+		# Position the label 10px in from the top-left
+		_fps_label.position = Vector2(10, 10)
+		fps_layer.add_child(_fps_label)
+
+func _process(delta: float) -> void:
+	if show_fps and _fps_label:
+		_fps_label.text = "FPS: %d\nAnts: %d" % [Engine.get_frames_per_second(), ant_base.antCount]
